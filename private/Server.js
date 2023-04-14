3bdfe99e32f920164c1eb0ee46af0babdb2ae87b
lamkhanhtran <tranphamlamkhanh@gmail.com>
2023-04-14 11:10:20 +0900

    add pictures

--- private/Server.js
+++ private/Server.js
@@ -14,7 +14,9 @@ const path = require( 'path' );                                 // Join director
 const helmet = require( 'helmet' );                             // Securing ‘express’ applications by setting various HTTP headers
                                                                 // It helps in mitigating cross-site scripting attacks, misissued SSL certificates, etc.
 
-const PORT = 8082;                                              // The port that this page is using
+const PORT = process.env.PORT || 8082;                                              // The port that this page is using
+
+const HOST = '0.0.0.0';
 
 ////////////////////////////////////////////////////////
 //const prequery_route = require( './Prequery-user' );//
@@ -28,7 +30,7 @@ const page_route_v1 = require( './v1-nodejs.js' );              // Contains rout
 
 const app = express( );                                         // Set up the app to be the main server itself
 
-var server = https.createServer( 
+/*var server = https.createServer( 
 
     // Provide the private key and public key to the server by reading each
     // file's content with the readFileSync() method.
@@ -39,7 +41,7 @@ var server = https.createServer(
     app
 
 );                                                              // Set up the server that is secured by the private key and the public key
-
+*/
 app.use( express.urlencoded( { extended: true } ) );            // We need express.urlencoded and express.json to
 app.use( express.json( ) );                                     // transform data received by server to become understandable by the server
 app.use( helmet( ) );                                           // protect https server from some ways of hacking
@@ -105,7 +107,7 @@ con.execute( sql, ["White"], function( error, result ) { // , fields) {
 } );
 */
 
-
+/*
 server.listen( PORT, function( error ) {                        // Listening through the port that we declared earlier
 
     if( error )                                                 // If cannot listen, return error
@@ -113,7 +115,7 @@ server.listen( PORT, function( error ) {                        // Listening thr
     console.log( 'Server listening on PORT', PORT );            // Printing that the server is created
 
 } );
-
+*/
 /*const express = require('express');
 const path = require('path');
 const app = express();
@@ -124,8 +126,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (request, response) {
     response.sendFile(path.join(__dirname, 'public/Users-types.html'))
 });
-
-app.listen(PORT, HOST, () => {
-    console.log(`Example app listening on port ${PORT}`)
-});
 */
+app.listen( PORT, HOST, ( error ) => {
+    if( error )                                                 // If cannot listen, return error
+        console.log( error );
+    console.log( `Server listening on PORT ${PORT}` );            // Printing that the server is created
+} );
